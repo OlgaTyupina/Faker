653495b363cee2e00a60b736d7dada5c02d252dd
Yuliya Gerashchenko <merg@list.ru>
2019-12-21 20:54:22 +0300

    Issue

---
+++ src/test/java/DataUse.java
@@ -0,0 +1,50 @@
+import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.openqa.selenium.Keys;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class DataUse {
+    private static Faker faker = new Faker(new Locale("ru"));
+    private static String firstName = faker.name().firstName();
+    private static String lastName = faker.name().lastName();
+    private static String phone = faker.phoneNumber().cellPhone();
+    private static String city = faker.address().cityName();
+
+    public static Faker getFaker() {
+        return faker;
+    }
+
+    public static String getName() {
+        return firstName+lastName;
+    }
+
+
+    public static String getPhone() {
+        return phone;
+    }
+
+    public static String getCity() {
+        return city;
+    }
+    public static String getFormattedFutureDate (){
+        LocalDate today = LocalDate.now().plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dayToMeet = today.format(formatter);
+        return dayToMeet;
+    }
+    public static void cleanUp(){
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.DELETE));
+    }
+    public static String getNewDate(){
+        LocalDate today = LocalDate.now().plusDays(10);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dayToMeet = today.format(formatter);
+        return dayToMeet;
+    }
+}
